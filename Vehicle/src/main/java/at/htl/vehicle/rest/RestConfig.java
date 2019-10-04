bbf42d5954364070117946a36de51efdc73d4b65
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-04 20:56:58 +0200

    created Vehicle

---
+++ Vehicle/src/main/java/at/htl/vehicle/rest/RestConfig.java
@@ -0,0 +1,10 @@
+package at.htl.vehicle.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfig extends Application {
+
+
+}
