fb815001d58a4dd4d39bbcd3531f459d4be143d9
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-11 13:25:41 +0200

    added servlet function

--- Vehicle/src/main/java/at/htl/vehicle/servlet/MyFirstServlet.java
+++ Vehicle/src/main/java/at/htl/vehicle/servlet/MyFirstServlet.java
@@ -1,4 +1,16 @@
 package at.htl.vehicle.servlet;
 
-public class MyFirstServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("my")
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String name = req.getParameter("name");
+        resp.getWriter().printf("Hello %s!", name);
+    }
 }
