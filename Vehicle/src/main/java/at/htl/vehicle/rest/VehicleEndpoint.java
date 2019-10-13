449958bfa310bd3e2b1275d22d895d5f520b71b9
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-10-13 11:43:48 +0200

    added delete and post

--- Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
+++ Vehicle/src/main/java/at/htl/vehicle/rest/VehicleEndpoint.java
@@ -2,10 +2,7 @@ package at.htl.vehicle.rest;
 
 import at.htl.vehicle.model.Vehicle;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +12,7 @@ public class VehicleEndpoint {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN, MediaType.APPLICATION_XML})
     public Vehicle find(@PathParam("id") long id) {
         return new Vehicle("Opel " + id, "Commodere");
     }
@@ -25,6 +22,18 @@ public class VehicleEndpoint {
         List<Vehicle> all = new ArrayList<>();
         all.add(find(42));
         return all;
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") long id) {
+        System.out.println("deleted: " + id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void save(Vehicle vehicle) {
+        System.out.println("Vehicle = " + vehicle);
     }
 
 }
