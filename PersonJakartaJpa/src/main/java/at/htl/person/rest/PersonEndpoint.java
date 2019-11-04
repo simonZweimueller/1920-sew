d3bccc12b113d1d91637da54e8a454d195832dd5
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-11-04 11:02:27 +0100

    added get

--- PersonJakartaJpa/src/main/java/at/htl/person/rest/PersonEndpoint.java
+++ PersonJakartaJpa/src/main/java/at/htl/person/rest/PersonEndpoint.java
@@ -1,4 +1,13 @@
 package at.htl.person.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("person")
 public class PersonEndpoint {
+
+    @GET
+    public String hello() {
+        return "hello";
+    }
 }
