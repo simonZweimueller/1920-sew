c03c928156700d0b9a2d7a1549b0ba1436835d49
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-11-04 10:48:28 +0100

    entity person created

--- PersonJakartaJpa/src/main/java/at/htl/person/model/Person.java
+++ PersonJakartaJpa/src/main/java/at/htl/person/model/Person.java
@@ -1,4 +1,53 @@
 package at.htl.person.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MY_PERSON")
 public class Person {
+
+    @Id
+    Long id;
+    String name;
+    String city;
+
+    public Person(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public Person() {
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s (%s)", id, name, city);
+    }
 }
