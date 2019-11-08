8eedf89ce4357233b0fecce771052b2daa636027
Simon Zweimueller <simon.zweimueller1@gmail.com>
2019-11-08 18:05:04 +0100

    updated InitBean

---
+++ PersonJakartaJpa/src/main/java/at/htl/person/InitBean.java
@@ -0,0 +1,46 @@
+package at.htl.person;
+
+import at.htl.person.model.Hobby;
+import at.htl.person.model.PerformsHobby;
+import at.htl.person.model.Person;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class InitBean {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Transactional
+    private void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+
+        em.persist(new Person("Susi", "Leonding"));
+        em.persist(new Person("Hansi", "Linz"));
+        em.persist(new Person("Mimi", "Leonding"));
+
+        em.persist(new Hobby("Mountain-Biken"));
+        em.persist(new Hobby("Lesen"));
+        em.persist(new Hobby("Bingen"));
+        em.persist(new Hobby("Ski fahren"));
+
+        em.persist(new PerformsHobby(
+                em.find(Hobby.class, 1L),
+                em.find(Person.class, 2L)
+        ));
+        em.persist(new PerformsHobby(
+                em.find(Hobby.class, 2L),
+                em.find(Person.class, 2L)
+        ));
+        em.persist(new PerformsHobby(
+                em.find(Hobby.class, 1L),
+                em.find(Person.class, 1L)
+        ));
+
+    }
+}
